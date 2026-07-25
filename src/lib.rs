2c343ad2050293e2517de30a4bf0668dd5e49642
kination <kination27@gmail.com>
2026-07-25 18:57:56 +0900

    Remove old features for new design process

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,6 @@
-mod context;
 pub mod debug;
 mod error;
-mod platform;
-mod program;
-pub mod protocol;
 mod stats;
 
-pub use context::{Action, Context, Mode, Rule};
 pub use error::FlightError;
-pub use program::Program;
 pub use stats::Stats;
